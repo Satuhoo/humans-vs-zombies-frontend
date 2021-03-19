9f90125236627d5ba979bb3af447c333ca8f1338
Satuhoo <satu@5CG05206R0.nordic.local>
2021-03-19 11:50:53 +0200

    feat: add params to game details view

--- src/components/pages/GameDetails.js
+++ src/components/pages/GameDetails.js
@@ -1,7 +1,18 @@
-function GameDetails() {
+import { useEffect, useState } from "react";
+import GameService from '../../services/game.service';
+
+function GameDetails(props) {
+    const id = props.match.params.id;
+    const [game, setGame] = useState({});
+    
+    useEffect(() => {
+        GameService.getGameByID(id)
+        .then(response => setGame(response.data))
+    }, [id])
+
     return (
         <div>
-            <h1>Game details</h1>
+            <h1>{game.name}</h1>
         </div>
     )
 }

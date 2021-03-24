23139b59bac53adbd2e5a9e59c156db795011409
okarp <ky6034@kolumbus.fi>
2021-03-24 14:13:09 +0200

    feat: testing keycloak

--- src/components/pages/GameDetails.js
+++ src/components/pages/GameDetails.js
@@ -1,59 +1,32 @@
 import { useEffect, useState } from "react";
 import { getGame } from '../../store/actions/gameActions';
 import { useDispatch, useSelector } from 'react-redux';
-import '../styles/GameDetails.css';
-import Map from '../map/Map';
-import Chat from '../chat/Chat';
-import BiteCodeForm from '../forms/BiteCodeForm';
-import Rules from '../games/Rules';
+import Keycloak from 'keycloak-js';
 
-function GameDetails(props) {
+function GameDetails(props) {    
     const id = props.match.params.id;
     const game = useSelector(state => state.gameReducer.game);
     const dispatch = useDispatch();
-    const [registered, setRegistered] = useState(false);
-    const [biteCode, setBiteCode] = useState('');
+    const [authenticated, setAuthenticated] = useState(false);
 
     useEffect(() => {
+        const keycloak = Keycloak('/keycloak.json');
+        keycloak.init({onLoad: 'login-required'}).then(authenticated => {            
+          setAuthenticated(authenticated);
+          console.log(keycloak.token);
+        })
         dispatch(getGame(id));
-        //TODO: check if the player is registered to the game
     }, [id, dispatch])
 
-    const handleRegistration = () => {
-        setRegistered(true);
-    }
-
-    const handleBiteCodeChange = (event) => {
-        setBiteCode(event.target.value);
-    }
-
-    const handleBite = (event) => {
-        event.preventDefault();
-        console.log('bite')
-    }
-
     return (
-        <div className="game-details-container">
-            <div>
-                <h2>{game.name}</h2>
-                <h3>Description</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-                    tempor incididunt ut labore et dolore magna aliqua. 
-                </p>
-                {registered ? <p><br></br>Player state</p>: <button onClick={handleRegistration}>Join</button>}
-                {registered && <BiteCodeForm biteCode={biteCode} onSubmit={handleBite} handleBiteCodeChange={handleBiteCodeChange}/>}
-            </div>
-            <div>
-                <Rules />
-                <h3>Location</h3>
-                <Map />
-            </div>
-            {registered && <div className="chat-container">
-                <h3>Chat</h3>
-                <Chat />
-            </div>}
-        </div>
-    )
+        <div> 
+        {authenticated &&  
+         <div>
+           <div>{game.name}</div>               
+         </div> 
+        }  
+  </div> 
+);
 }
 
 export default GameDetails;

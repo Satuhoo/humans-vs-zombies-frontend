1dbdef945f0f083c404995adaac61b92144b64f5
okarp <ky6034@kolumbus.fi>
2021-03-24 17:02:44 +0200

    test: testing game details guarding

--- src/components/pages/GameDetails.js
+++ src/components/pages/GameDetails.js
@@ -1,8 +1,11 @@
 import { useEffect } from "react";
 import { getGame } from '../../store/actions/gameActions';
 import { useDispatch, useSelector } from 'react-redux';
+import {Link} from "react-router-dom";
+import { useKeycloak } from '@react-keycloak/web'
 
 function GameDetails(props) {
+    const { keycloak } = useKeycloak();
     const id = props.match.params.id;
     const game = useSelector(state => state.gameReducer.game);
     const dispatch = useDispatch();
@@ -13,8 +16,19 @@ function GameDetails(props) {
 
     return (
         <div>
-            <h1>{game.name}</h1>
+            {keycloak.authenticated &&
+                <div>
+                <h1>{game.name}</h1>
+            </div>            
+            }
+        {!keycloak.authenticated &&
+                <div>
+                <h1>You have to be logged in to see this page.</h1>
+                <Link to="/login">Click here to login</Link>
+            </div>            
+            }        
         </div>
+    
     )
 }
 

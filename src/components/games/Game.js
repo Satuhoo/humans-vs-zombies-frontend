fc884c04e8ae5bd9eb1d004d56bec895417af510
Maija Haka <maija.haka@live.fi>
2021-03-25 10:54:06 +0200

    feat: show game state and number of players for each game on the landing page

--- src/components/games/Game.js
+++ src/components/games/Game.js
@@ -6,6 +6,7 @@ function Game({game, gameClicked}) {
     return (
         <div onClick={handleClickGame}>
             <h2>{game.name}</h2>
+            <p>{game.gameState} Players: {game.players.length}</p>
         </div>
     )
 }

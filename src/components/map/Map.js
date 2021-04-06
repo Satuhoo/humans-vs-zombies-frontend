677e5f0e96c6d06d8e438407ff38e3bfde883bd6
Maija Haka <maija.haka@live.fi>
2021-04-06 20:06:12 +0300

    fix: remove kills from UseEffect dependencies in Map to avoid infinite get loop

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -12,7 +12,7 @@ function Map(props) {
 
     useEffect(() => {
       dispatch(getKills(props.game.id));      
-    }, [props.game.id, dispatch, kills])
+    }, [props.game.id, dispatch])
 
     const onReceiveMessage = gameId => {
       if (gameId === props.game.id) {

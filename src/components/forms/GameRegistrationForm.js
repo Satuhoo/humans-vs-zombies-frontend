4fc73ce8be789d7fad9782fc00a488057ccb3b97
Satuhoo <satu@5CG05206R0.nordic.local>
2021-04-06 16:46:14 +0300

    feat: add story for kill and customize forms in game details page

--- src/components/forms/GameRegistrationForm.js
+++ src/components/forms/GameRegistrationForm.js
@@ -3,9 +3,10 @@ import Button from 'react-bootstrap/Button';
 
 function GameRegistrationForm({playerName, handlePlayerNameChange, handleRegistration}) {
     return (
-        <div>
-            <Form.Label className="form-label">Player name</Form.Label>
-                <Form.Control value={playerName} onChange={handlePlayerNameChange} />
+        <div className="form-container">
+            <Form.Label className="form-label">Your player name</Form.Label>
+            <Form.Control value={playerName} onChange={handlePlayerNameChange} />
+            <br/>
             <Button variant="info" onClick={handleRegistration}>Join</Button>
         </div>
     )
--- src/components/pages/GameDetails.js
+++ src/components/pages/GameDetails.js
@@ -27,6 +27,7 @@ function GameDetails(props) {
     const dispatch = useDispatch();
     const [registered, setRegistered] = useState(false);
     const [biteCode, setBiteCode] = useState('');
+    const [story, setStory] = useState('');
     const [showEditView, setShowEditView] = useState(false);
     const [playerName, setPlayerName] = useState('');
     const [loading, setLoading] = useState(true);
@@ -68,6 +69,10 @@ function GameDetails(props) {
         setBiteCode(event.target.value);
     }
 
+    const handleStoryChange = (event) => {
+        setStory(event.target.value);
+    }
+
     const handlePlayerNameChange = (event) => {
         setPlayerName(event.target.value);
     }
@@ -79,11 +84,13 @@ function GameDetails(props) {
             const kill = {
                 'biteCode': biteCode,
                 'killerId': player.id,
+                'story': story,
                 'lat': position.coords.latitude,
                 'lng': position.coords.longitude
             }
-            dispatch(killPlayer(game.id, kill))
-            setBiteCode('')
+            dispatch(killPlayer(game.id, kill));
+            setBiteCode('');
+            setStory('');
         })
     }
 
@@ -143,13 +150,14 @@ function GameDetails(props) {
                     {/* Checks if user is an admin, if user is registered to the game and the player 
                         state. Shows the right view depending on those*/}
                     {!user.isAdmin ? <div className="grid-item item3">
-                        {registered ? <p><br></br>Player id {player.id}</p>: 
+                        {!registered &&
                             <GameRegistrationForm playerName={playerName} handlePlayerNameChange={handlePlayerNameChange} 
                                 handleRegistration={handleRegistration} />}
                         {registered && !showEditView && 
                                 (player.human 
                                     ? <div>Your bite code is <b>{player.biteCode}</b></div>
-                                    : <BiteCodeForm biteCode={biteCode} onSubmit={handleBite} handleBiteCodeChange={handleBiteCodeChange}/>
+                                    : <BiteCodeForm biteCode={biteCode} story={story} onSubmit={handleBite} 
+                                    handleBiteCodeChange={handleBiteCodeChange} handleStoryChange={handleStoryChange}/>
                                 )
                         }    
                     </div>

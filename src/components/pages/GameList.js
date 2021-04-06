e3c01fe0c2d7ccb19d69d3399d8909f7805f6329
Maija Haka <maija.haka@live.fi>
2021-04-06 12:35:05 +0300

    feat: SockJsClient in GameList dispatches getGames() action when it receives a message

--- src/components/pages/GameList.js
+++ src/components/pages/GameList.js
@@ -4,6 +4,7 @@ import { useHistory } from 'react-router-dom';
 import { getGames, clearGame } from '../../store/actions/gameActions';
 import { useDispatch, useSelector } from 'react-redux';
 import { useKeycloak } from '@react-keycloak/web';
+import SockJsClient from 'react-stomp';
 import AddGame from '../admin/AddGame';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
@@ -45,20 +46,30 @@ function GameList() {
         setShowAddGameButton(true);
     }
 
+    const onReceiveMessage = () => {
+        dispatch(getGames())
+    }
+
     return (
-        <div className="game-list-container">
-            <div>
-            <h1>Games</h1>
-            {games.map((game) => 
-                <Game game={game} key={game.id} gameClicked={handleClickGame}/>
-            )}
-            </div>
-            <div>
-                {!keycloak.authenticated && <p>Please <Link className="link" to="/login">Login</Link></p>}
-                {user.isAdmin && <div>
-                    {showAddGameButton ? <Button variant="info" onClick={handleClickAddGame}>Add game</Button>:
-                    <AddGame hideForm={hideForm} latitude={currentLatitude} longitude={currentLongitude} /> }
-                </div> }
+        <div>
+            <SockJsClient url='http://localhost:8080/ws' 
+                topics={["/topic/addGame"]}
+                onMessage={ () => onReceiveMessage() }
+            />
+            <div className="game-list-container">
+                <div>
+                <h1>Games</h1>
+                {games.map((game) => 
+                    <Game game={game} key={game.id} gameClicked={handleClickGame}/>
+                )}
+                </div>
+                <div>
+                    {!keycloak.authenticated && <p>Please <Link className="link" to="/login">Login</Link></p>}
+                    {user.isAdmin && <div>
+                        {showAddGameButton ? <Button variant="info" onClick={handleClickAddGame}>Add game</Button>:
+                        <AddGame hideForm={hideForm} latitude={currentLatitude} longitude={currentLongitude} /> }
+                    </div> }
+                </div>
             </div>
         </div>
     )
